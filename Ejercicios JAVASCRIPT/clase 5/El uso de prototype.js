bf8f5fd07eaa552b9e9631be4ae153834d76d530
ivo <rossiivomartin@gmail.com>
2023-05-30 20:48:33 -0300

    clase5

---
+++ Ejercicios JAVASCRIPT/clase 5/El uso de prototype.js
@@ -0,0 +1,6 @@
+//Uso de prototype
+persona3.prototype.telefono = '2618383832';
+console.log(padre); ... na3 { nombre: 'Luis', apellido: 'Lopez', email: 'lopezl@gmail.com', nombrecompleto:[λ], telefono: '5492618282821'}
+console.log(madre.telefono); 2618383832
+madre.telefono = '5492618383832'
+console.log(madre.telefono); 5492618383832
