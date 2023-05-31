1d99e16be28bcdfe26f3769095f5b3d02ae615cd
Six087 <112667854+Six087@users.noreply.github.com>
2023-05-30 21:45:07 -0300

    Trabajos Clase 1 JS
    Trabajos de la semana 1 de JS

---
+++ Ejercicios JAVASCRIPT/Clase 1/dowhile.js
@@ -0,0 +1,12 @@
+let cont = 0;
+do{
+    console.log(cont);
+    cont++;
+}while(cont < 3);
+console.log("Fin del ciclo");
+
+//for 
+for( let contando = 0; contando < 3; contando++){
+    console.log(contando);
+}
+console.log("Fin del ciclo for");
---
+++ Ejercicios JAVASCRIPT/Clase 1/while.js
@@ -0,0 +1,6 @@
+let cont = 0;
+while(cont < 3){
+    console.log(cont);
+    contador++;
+}
+console.log("Fin del ciclo while");
