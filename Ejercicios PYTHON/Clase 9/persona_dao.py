59f25f7fe8dd457cc41d4a2319317e6f297e53c2
FacundoGerv <facugervasi9@gmail.com>
2023-06-13 23:32:44 -0300

    Testing git stuff

--- Ejercicios PYTHON/Clase 9/persona_dao.py
+++ Ejercicios PYTHON/Clase 9/persona_dao.py
@@ -1,7 +1,6 @@
 from capa_datos_persona.Persona import Persona
 from capa_datos_persona.Conexion import Conexion
 from logger_base import log
-
 class PersonaDAO:
     """
     DAO significa: Data Access Object
@@ -15,7 +14,6 @@ class PersonaDAO:
     _INSERTAR =  'INSERT INTO persona(nombre, apellido, email) VALUES (%s, %s, %s'
     _ACTUALIZAR = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
     _ELIMINAR = 'DELETE FROM persona WHERE id_persona=%s'
-    
     #Definimos los metodos de clase
     @classmethod
     def seleccionar(cls):
@@ -27,8 +25,7 @@ class PersonaDAO:
                 for registros in registros:
                     persona = Persona(registro[0], registro[1], registro[2], registro[3])
                     persona.append(persona)
-                return personas
-                    
+                return personas          
     @classmethod
     def insertar(cls, persona):
         with Conexion.obtenerCursor():
@@ -36,8 +33,7 @@ class PersonaDAO:
                 valores = (persona.nombre, persona.apellido, persona.email)
                 cursor.execute(cls._INSERTAR, valores)
                 log.debug(f'Persona Insertada: {persona}')
-                return cursor.rowcount
-                    
+                return cursor.rowcount           
     @classmethod
     def actualizar(cls, persona):
         with Conexion.obtenerConexion():
@@ -45,17 +41,14 @@ class PersonaDAO:
             valores = (persona.nombre, persona.apellido, persona.email, persona.id_persona)
             cursor.execute(cls._ACTUALIZAR, valores)
             log.debug(f'Persona actualizada: {persona}')
-            return cursor.rowcount
-                    
+            return cursor.rowcount                
     @classmethod
     def eliminar(cls, persona):
         with Conexion.obtenerCursor() as cursor:
             valores = (persona.id_persona,)
             cursor.execute(cls._ELIMINAR, valores)
             log.debug(f'Los objetos eliminados son: {persona}')
-            return cursor.rowcount
-                    
-                    
+            return cursor.rowcount              
 if __name__ == '__main__':
     #Eliminar un registro
     # persona1 = Persona(id_persona=8)
